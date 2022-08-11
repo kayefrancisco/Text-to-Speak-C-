08af73f6711496a4b1809dc8aa2f66116668e163
kayefrancisco <106609187+kayefrancisco@users.noreply.github.com>
2022-08-10 22:50:41 -0700

    Add files via upload

---
+++ Text To Speak_code/Text To Speak/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Text_To_Speak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Speak = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Resume = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(330, 135);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Speak
+            // 
+            this.Speak.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Speak.Location = new System.Drawing.Point(12, 163);
+            this.Speak.Name = "Speak";
+            this.Speak.Size = new System.Drawing.Size(78, 28);
+            this.Speak.TabIndex = 1;
+            this.Speak.Text = "Speak";
+            this.Speak.UseVisualStyleBackColor = false;
+            this.Speak.Click += new System.EventHandler(this.Speak_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Stop.Location = new System.Drawing.Point(264, 163);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(78, 28);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Resume
+            // 
+            this.Resume.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Resume.Location = new System.Drawing.Point(180, 163);
+            this.Resume.Name = "Resume";
+            this.Resume.Size = new System.Drawing.Size(78, 28);
+            this.Resume.TabIndex = 3;
+            this.Resume.Text = "Resume";
+            this.Resume.UseVisualStyleBackColor = false;
+            this.Resume.Click += new System.EventHandler(this.Resume_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Pause.Location = new System.Drawing.Point(96, 163);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(78, 28);
+            this.Pause.TabIndex = 4;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = false;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(353, 211);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Resume);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Speak);
+            this.Controls.Add(this.richTextBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Text To Speak";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Speak;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Resume;
+        private System.Windows.Forms.Button Pause;
+    }
+}
+
---
+++ Text To Speak_code/Text To Speak/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace Text_To_Speak
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Speak_Click(object sender, EventArgs e)
+        {
+            if(richTextBox1.Text!=""){
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                reader.SpeakAsync(richTextBox1.Text);
+        }
+            else{
+                MessageBox.Show("Please enter some Text First!");
+            }
+
+    }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            if(reader!=null)
+            {
+                if(reader.State==SynthesizerState.Speaking)
+                {
+                reader.Pause();
+                }
+            }
+        }
+
+        private void Resume_Click(object sender, EventArgs e)
+        {
+         if(reader!=null){
+                if(reader.State==SynthesizerState.Paused){
+                reader.Resume();
+                }
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                reader.Dispose();
+            }
+        }
+
+        
+     }
+}
